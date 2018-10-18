96d2a2f3b45d355423e0a63a6ae7750d3ff51437
zan_od <zan_od@mail.ru>
2018-10-18 23:54:00 +0300

    Initial commit

---
+++ src/main/java/com/example/guessgame/GuessGameApplication.java
@@ -0,0 +1,12 @@
+package com.example.guessgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessGameApplication.class, args);
+    }
+}
